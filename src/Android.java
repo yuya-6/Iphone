ae1984639c92dca1cf3d2941f01ecc22f8777e05
yuya-6 <yuchongakufu@gmail.com>
2024-03-31 21:06:26 +0900

    継承

---
+++ src/Android.java
@@ -0,0 +1,18 @@
+//演習④
+public class Android {
+	
+	public static void main(String[] args){
+		
+		SmartPhone android = new SmartPhone();
+		android.play();
+		android.stop();
+		android.next();
+		android.back();
+		android.call();
+		android.mail();
+		android.photo();
+		android.internet();
+		
+	}
+
+}
---
+++ src/Iphone.java
@@ -0,0 +1,18 @@
+//演習③
+public class Iphone {
+	
+	public static void main(String[] args){
+		
+		SmartPhone iphone = new SmartPhone();
+		iphone.play();
+		iphone.stop();
+		iphone.next();
+		iphone.back();
+		iphone.call();
+		iphone.mail();
+		iphone.photo();
+		iphone.internet();
+		
+	}
+
+}
